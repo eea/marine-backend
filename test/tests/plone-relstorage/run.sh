05fbf65706e2565454f0ddf654dd0f1a9e2b50d0
nileshgulia1 <nileshgulia@gmail.com>
2026-03-31 20:10:44 +0530

    fix(tests): set plone_test_retries to 20

--- test/tests/plone-relstorage/run.sh
+++ test/tests/plone-relstorage/run.sh
@@ -6,7 +6,7 @@ dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 image="$1"
 
 PLONE_TEST_SLEEP=10
-PLONE_TEST_TRIES=10
+PLONE_TEST_TRIES=20
 
 # Start Postgres
 zname="relstorage-container-$RANDOM-$RANDOM"
